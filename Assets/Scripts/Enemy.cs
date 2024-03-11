27a4817d59a3bfa9c90f2bc42cdda4d72b4f3a4c
WonKangHee <rkdgml0820@korea.ac.kr>
2024-03-11 11:36:18 +0900

    Feat: 적 장애물 감지 기능 구현

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,30 +9,73 @@ public class Enemy : MonoBehaviour
 
     private CircleCollider2D circleCollider;
 
+    private Transform player;
+
     [SerializeField]
     private float coolTime;
     private bool isCool;
+    private bool isPlayerDetected;
 
     private void Awake()
     {
         circleCollider = GetComponent<CircleCollider2D>();
     }
 
+    private void Start()
+    {
+        isCool = false;
+        isPlayerDetected = false;
+    }
+
+    private void Update()
+    {
+        if (isAttackPossible()) Attack(player);
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            //if (isCool) return;
-            Attack(other.transform.position);
+            if (IsTargetVisible(other.transform) && isPlayerDetected == false)
+            {
+                isPlayerDetected = true;
+                player = other.transform;
+            }
         }
     }
 
-    private Vector2 getTargetDir(Vector2 target) 
-        => (target - (Vector2)transform.position).normalized;
-
-    protected virtual void Attack(Vector2 target)
+    private bool IsTargetVisible(Transform target)
     {
-        var dir = getTargetDir(target);
+        Vector2 dir = GetTargetDir(target);
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dir,
+                                                circleCollider.radius, GetObstacleLayerMask());
+        
+        //Debug
+        if (rayHit) Debug.DrawLine(transform.position, rayHit.point, Color.yellow);
+        else Debug.DrawLine(transform.position, target.position, Color.red);
 
-    } 
+        if (rayHit) return false;
+        else return true;
+    }
+
+    private bool isAttackPossible() => (isPlayerDetected && !isCool);
+
+    protected virtual void Attack(Transform target)
+    {
+        var dir = GetTargetDir(target);
+        //Implement Attack
+    }
+
+    private Vector2 GetTargetDir(Transform target)
+        => (target.position - transform.position).normalized;
+
+    public LayerMask GetObstacleLayerMask()
+    {
+        LayerMask newLayerMask = viewObstacleMask[0];
+        foreach (var layerMask in viewObstacleMask)
+        {
+            newLayerMask = newLayerMask | layerMask;
+        }
+        return newLayerMask;
+    }
 }
