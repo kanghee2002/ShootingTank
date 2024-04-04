02c96c2ade5cded82efea885aaf2102669758fc1
WonKangHee <rkdgml0820@korea.ac.kr>
2024-04-04 16:14:22 +0900

    Refactor: 적의 플레이어 감지 기능 하위 오브젝트로 분리
    하위 오브젝트 PlayerDetector 구현

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,21 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Enemy : ShootingObject, IDamageable
 {
-    [SerializeField]
-    private List<LayerMask> viewObstacleMask = new();
-
-    private CircleCollider2D circleCollider;
 
     private Transform player;
+    public Transform Player { get => player; set => player = value; }
 
     [SerializeField]
     private float coolTime;
 
     private bool isCool;
     private bool isPlayerDetected;
+    public bool IsPlayerDetected { get => isPlayerDetected; set => isPlayerDetected = value; }
 
     IDamageable damageableInstance;
 
@@ -25,7 +23,6 @@ public abstract class Enemy : ShootingObject, IDamageable
 
     private void Awake()
     {
-        circleCollider = GetComponent<CircleCollider2D>();
         damageableInstance = this;
     }
 
@@ -38,42 +35,6 @@ public abstract class Enemy : ShootingObject, IDamageable
     private void Update()
     {
         if (IsAttackPossible()) Attack(player);
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (IsTargetVisible(other.transform) && isPlayerDetected == false)
-            {
-                isPlayerDetected = true;
-                player = other.transform;
-            }
-        }
-    }
-
-    public LayerMask GetObstacleLayerMask()
-    {
-        LayerMask newLayerMask = viewObstacleMask[0];
-        foreach (var layerMask in viewObstacleMask)
-        {
-            newLayerMask |= layerMask;
-        }
-        return newLayerMask;
-    }
-
-    private bool IsTargetVisible(Transform target)
-    {
-        var dir = GetTargetDir(target);
-        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dir,
-                                                circleCollider.radius, GetObstacleLayerMask());
-        
-        //Debug
-        if (rayHit) Debug.DrawLine(transform.position, rayHit.point, Color.yellow);
-        else Debug.DrawLine(transform.position, target.position, Color.red);
-
-        if (rayHit) return false;
-        else return true;
     }
 
     private bool IsAttackPossible() => (isPlayerDetected && !isCool);
---
+++ Assets/Scripts/Enemy/PlayerDetector.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    [SerializeField]
+    private List<LayerMask> viewObstacleMasks = new();
+
+    private CircleCollider2D circleCollider;
+    private Enemy enemy;
+    private LayerMask compositedObstacleMask;
+
+    private void Awake()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        enemy = transform.parent.GetComponent<Enemy>();
+        compositedObstacleMask = GetObstacleLayerMask();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (IsTargetVisible(other.transform) && enemy.IsPlayerDetected == false)
+            {
+                enemy.IsPlayerDetected = true;
+                enemy.Player = other.transform;
+            }
+        }
+    }
+
+    private LayerMask GetObstacleLayerMask()
+    {
+        LayerMask newLayerMask = viewObstacleMasks[0];
+        foreach (var layerMask in viewObstacleMasks)
+        {
+            newLayerMask |= layerMask;
+        }
+        return newLayerMask;
+    }
+
+    private bool IsTargetVisible(Transform target)
+    {
+        var dir = GetTargetDir(target);
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dir,
+                                                circleCollider.radius, compositedObstacleMask);
+
+        //Debug
+        if (rayHit) Debug.DrawLine(transform.position, rayHit.point, Color.yellow);
+        else Debug.DrawLine(transform.position, target.position, Color.red);
+
+        if (rayHit) return false;
+        else return true;
+    }
+
+    private Vector3 GetTargetDir(Transform target)
+        => (target.position - transform.position).normalized;
+}
