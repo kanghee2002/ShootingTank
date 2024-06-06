d858b41ade82b1d098f457235a1109f966624916
kanghee02 <108472294+kanghee2002@users.noreply.github.com>
2024-06-06 12:41:17 +0900

    Feat: 점프팩 구현
    점프팩 게이지 보여줄 UI 필요

---
+++ Assets/Scripts/PlayerUtility/JumpPack.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpPack : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private PlayerController playerController;
+
+    [SerializeField]
+    private float fallSpeed;
+
+    [SerializeField]
+    private float maxGauge;
+    private float curGauge;
+
+    [SerializeField]
+    private float useSpeed;
+
+    [SerializeField]
+    private float fillInterval;
+
+    [SerializeField]
+    private float fillSpeed;
+
+    private IEnumerator curFillCoroutine;
+
+
+
+    //Re
+    private Slider slider;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+        rigid = GetComponent<Rigidbody2D>();
+        curGauge = maxGauge;
+
+
+        //Re
+        slider = GameObject.Find("JumpPackSlider").GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        //Re
+        slider.value = curGauge / maxGauge;
+
+
+
+
+        if (rigid.velocity.y < 0 && Input.GetKey(KeyCode.Space) && curGauge > 0f)
+        {
+            if (curFillCoroutine != null)
+            {
+                StopCoroutine(curFillCoroutine);
+                curFillCoroutine = null;
+            }
+            Use();
+        }
+        else
+        {
+            if (curFillCoroutine == null)
+            {
+                curFillCoroutine = FillGauge();
+                StartCoroutine(curFillCoroutine);
+            }
+        }
+    }
+
+    private void Use()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, fallSpeed);
+        curGauge -= useSpeed;
+    }
+
+    private IEnumerator FillGauge()
+    {
+        yield return new WaitForSeconds(fillInterval);
+
+        while (curGauge < maxGauge)
+        {
+            curGauge += fillSpeed;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
