04756dff9f876c0a4f871636107e078e8d615b02
kanghee02 <108472294+kanghee2002@users.noreply.github.com>
2024-07-05 09:18:08 +0900

    Feat: 받는 코어 데미지 증가 유틸리티

--- Assets/Scripts/Player/CoreHealth.cs
+++ Assets/Scripts/Player/CoreHealth.cs
@@ -8,11 +8,14 @@ public class CoreHealth : Health
     [SerializeField] Health parentHealth;
 
     [SerializeField] private float coreDamageMultiplier;
-    public float CoreDamageMultiplier { get => coreDamageMultiplier; set => coreDamageMultiplier = value;  }
 
     public override void TakeDamage(float damageAmount)
     {
         parentHealth.TakeDamage(damageAmount * coreDamageMultiplier);
     }
+
+    public void AddCoreDamageMultiplier(float amount) => coreDamageMultiplier += amount;
+
+    public void MinusCoreDamageMultiplier(float amount) => coreDamageMultiplier -= amount;
 
 }
---
+++ Assets/Scripts/PlayerUtility/CoreTakeDamageMultiplerAdder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreTakeDamageMultiplerAdder : MonoBehaviour
+{
+    [SerializeField]  private float coreTakeDamageMultiplerBonus;
+
+    private CoreHealth[] coreHealthArray;
+
+    private void Awake()
+    {
+        coreHealthArray = GetComponentsInChildren<CoreHealth>();
+    }
+
+    private void OnEnable()
+    {
+        foreach (CoreHealth coreHealth in coreHealthArray)
+        {
+            coreHealth.AddCoreDamageMultiplier(coreTakeDamageMultiplerBonus);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (CoreHealth coreHealth in coreHealthArray)
+        {
+            coreHealth.MinusCoreDamageMultiplier(coreTakeDamageMultiplerBonus);
+        }
+    }
+}
