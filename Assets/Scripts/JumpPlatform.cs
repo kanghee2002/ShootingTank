e2826436d17deb6fd305d2715a85dad9c84b3c1b
KangHee <rkdgml0820@naver.com>
2024-07-11 11:01:52 +0900

    Feat: Jump Platform 구현

---
+++ Assets/Scripts/JumpPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class JumpPlatform : MonoBehaviour
+{
+    [SerializeField] private float jumpPowerBonus;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Settings.playerTag))
+        {
+            if (collision.TryGetComponent(out PlayerController playerController))
+            {
+                playerController.AddJumpPowerValue(jumpPowerBonus);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerController playerController))
+        {
+            playerController.MinusJumpPowerValue(jumpPowerBonus);
+        }
+    }
+}
