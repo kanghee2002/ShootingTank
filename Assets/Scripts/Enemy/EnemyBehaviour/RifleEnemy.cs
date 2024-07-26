06db6afc74ee565460dd47718b2cf748513312f6
KangHee <rkdgml0820@naver.com>
2024-07-26 11:21:19 +0900

    Feat: 연사 적 구현
    Television_Rifle 추가

---
+++ Assets/Scripts/Enemy/EnemyBehaviour/RifleEnemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RifleEnemy : Enemy
+{
+    [Header("Attack Settings")]
+    [SerializeField] private float bulletSpeed;
+
+    [SerializeField] private float weaponLength;
+
+    [SerializeField] private int pelletCount;
+
+    [SerializeField] private float fireInterval;
+
+    private int count = 0;
+
+    public override bool Attack(Transform playerTransform)
+    {
+        if (isCool)
+        {
+            return false;
+        }
+
+
+        Vector3 direction = GetTargetDirection(playerTransform);
+
+        var obj = objectPool.GetBullet();
+        obj.transform.position = transform.position + direction * weaponLength;
+        obj.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        Bullet bullet = obj.GetComponent<Bullet>();
+        bullet.LookAtDirection(obj, direction);
+        bullet.FinalDamage = damageValue;
+        bullet.AddTargetTag(Settings.playerTag);
+
+        count++;
+
+        isCool = true;
+
+        if (count == pelletCount)
+        {
+            count = 0;
+            StartCoroutine(CoolDownRoutine(coolTime));
+        }
+        else
+        {
+            StartCoroutine(CoolDownRoutine(fireInterval));
+        }
+
+        return true;
+    }
+}
