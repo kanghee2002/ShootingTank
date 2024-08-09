4969ecc2ff5140c15dea70a66f063b91e0dbabda
KangHee <rkdgml0820@naver.com>
2024-08-09 14:28:32 +0900

    Feat: 꼬리 총, 무한의 대총 프리팹 추가

--- Assets/Scripts/Misc/Enums.cs
+++ Assets/Scripts/Misc/Enums.cs
@@ -53,7 +53,7 @@ public enum WeaponName
     Sniper,
 
     //S
-    Taser
+    Infinitygun
 }
 
 public enum RoomType
--- Assets/Scripts/Weapon/Tailgun.cs
+++ Assets/Scripts/Weapon/Tailgun.cs
@@ -4,18 +4,80 @@ using UnityEngine;
 
 public class Tailgun : Weapon, IMultiFiregun
 {
+    [Header("Big Bullet")]
+    [SerializeField] private float maxBigBulletSize;
+    [SerializeField] private float bigBulletSize;
+
+    [Header("Small Bullet")]
+    [SerializeField] private float maxSmallBulletSize;
+    [SerializeField] private float smallBulletSize;
+
+    [Header("Burst")]
+    [SerializeField] private int pelletCount;
+    [SerializeField] private float burstTime;
+
+    private Coroutine currentShootingRoutine;
+
     public override void Fire(Vector3 direction, float chargeDamageMultiplierBonus, float maxChargedDamageBonus)
     {
-        throw new System.NotImplementedException();
+        if (CurAmmo <= 0)
+        {
+            Debug.Log(name + " : No Ammo");
+            return;
+        }
+
+        currentShootingRoutine = StartCoroutine(BurstFireRoutine(direction, chargeDamageMultiplierBonus, maxChargedDamageBonus));
+
+        CurAmmo--;
+
+        Recharge();
+    }
+
+    private IEnumerator BurstFireRoutine(Vector3 direction, float chargeDamageMultiplierBonus, float maxChargedDamageBonus)
+    {
+        float burstIntervalTime = burstTime / pelletCount;
+
+        float damageAmount = damageValue * GetDamageMultiplier(ChargePercentage, chargeDamageMultiplierBonus, maxChargedDamageBonus);
+
+        GameObject obj = objectPool.GetBullet();
+        Vector3 randomDirection = GetRandomDirection(direction, AimAccuracy);
+        Bullet bullet = obj.GetComponent<Bullet>();
+
+        SetBullet(obj, bullet, randomDirection, damageAmount);
+
+        obj.transform.localScale = Vector3.one * bigBulletSize;
+
+        yield return new WaitForSeconds(burstIntervalTime);
+
+        for (int i = 0; i < pelletCount; i++)
+        {
+            obj = objectPool.GetBullet();
+            randomDirection = GetRandomDirection(direction, AimAccuracy);
+            bullet = obj.GetComponent<Bullet>();
+
+            SetBullet(obj, bullet, randomDirection, damageAmount * 0.6f);
+
+            obj.transform.localScale = Vector3.one * smallBulletSize;
+
+            yield return new WaitForSeconds(burstIntervalTime);
+        }
     }
 
     void IDefaultgun.IncreaseBulletsize(float amount)
     {
-        throw new System.NotImplementedException();
+        if (bigBulletSize < maxBigBulletSize)
+        {
+            bigBulletSize += amount;
+
+            if (bigBulletSize > maxBigBulletSize)
+            {
+                bigBulletSize = maxBigBulletSize;
+            }
+        }
     }
 
     void IMultiFiregun.IncreasePelletCount(int count)
     {
-        throw new System.NotImplementedException();
+        pelletCount += count;
     }
 }
